c423de2279f94c10fc4ff48fd144f85e1c5257d6
Guilherme Canavarollo Lanza da Silva | S2IT <guilherme.lanza@S2IT.CORP>
2019-01-14 15:02:48 -0200

    aula 30

--- CursoSelenium01/src/main/java/TesteGoogle.java
+++ CursoSelenium01/src/main/java/TesteGoogle.java
@@ -1,22 +1,22 @@
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteGoogle {
 
 	@Test
 	public void teste() {
-		System.setProperty("webdriver.chrome.driver", "C:/WSCursoWebdriver/chromedriver.exe");		
-		//System.setProperty("webdriver.gecko.driver", "C:/WSCursoWebdriver/geckodriver.exe");
-		//WebDriver driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "C:/WSCursoWebdriver/chromedriver.exe");
+		// System.setProperty("webdriver.gecko.driver",
+		// "C:/WSCursoWebdriver/geckodriver.exe");
+		// WebDriver driver = new FirefoxDriver();
 		WebDriver driver = new ChromeDriver();
-		driver.manage().window().setPosition(new Point(100, 100));
-		driver.get("http://www.google.com.br");
-		System.out.println(driver.getTitle());
+		// driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.manage().window().maximize();
+		driver.get("http://www.google.com.br");		
 		Assert.assertEquals("Google", driver.getTitle().toString());
+		driver.quit(); //fecha tudo -> close fecha só a aba
 	}
 
 }
