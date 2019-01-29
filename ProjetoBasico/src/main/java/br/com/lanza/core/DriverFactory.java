a5510288225fb0b5adcadb126fa3cf1c477e330d
Guilherme Canavarollo Lanza da Silva | S2IT <guilherme.lanza@S2IT.CORP>
2019-01-29 15:28:36 -0200

    final do curso :)

--- ProjetoBasico/src/main/java/br/com/lanza/core/DriverFactory.java
+++ ProjetoBasico/src/main/java/br/com/lanza/core/DriverFactory.java
@@ -1,8 +1,15 @@
 package br.com.lanza.core;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import br.com.lanza.core.Propriedades.TipoExecucao;
 
 public class DriverFactory {
 
@@ -23,16 +30,57 @@ public class DriverFactory {
 
 	public static WebDriver initDriver() {
 		WebDriver driver = null;
-		switch (Propriedades.browser) {
-		case FIREFOX:
-			System.setProperty("webdriver.gecko.driver", "C:/WSCursoWebdriver/geckodriver.exe");
-			driver = new FirefoxDriver();
-			break;
-		case CHROME:
-			System.setProperty("webdriver.chrome.driver", "C:/WSCursoWebdriver/chromedriver.exe");
-			driver = new ChromeDriver();
-			break;
+		if (Propriedades.TIPO_EXECUCAO == TipoExecucao.LOCAL) {
+			switch (Propriedades.BROWSER) {
+			case FIREFOX:
+				System.setProperty("webdriver.gecko.driver", "C:/WSCursoWebdriver/geckodriver.exe");
+				driver = new FirefoxDriver();
+				break;
+			case CHROME:
+				System.setProperty("webdriver.chrome.driver", "C:/WSCursoWebdriver/chromedriver.exe");
+				driver = new ChromeDriver();
+				break;
+			}
 		}
+
+		if (Propriedades.TIPO_EXECUCAO == TipoExecucao.GRID) {
+			DesiredCapabilities cap = null;
+			switch (Propriedades.BROWSER) {
+			case FIREFOX:
+				cap = DesiredCapabilities.firefox();
+				break;
+			case CHROME:
+				cap = DesiredCapabilities.chrome();
+				break;
+			}
+
+			try {
+				driver = new RemoteWebDriver(new URL("http://192.168.0.184:4444/wd/hub"), cap);
+			} catch (MalformedURLException e) {
+				System.out.println("Erro de conexão com o GRID.");
+				e.printStackTrace();
+			}
+		}
+		
+		if (Propriedades.TIPO_EXECUCAO == TipoExecucao.NUVEM) {
+			DesiredCapabilities cap = null;
+			switch (Propriedades.BROWSER) {
+			case FIREFOX:
+				cap = DesiredCapabilities.firefox();
+				break;
+			case CHROME:
+				cap = DesiredCapabilities.chrome();
+				break;
+			}
+
+			try {
+				driver = new RemoteWebDriver(new URL("http://usuario:key@endereco:porta/wd/hub"), cap);
+			} catch (MalformedURLException e) {
+				System.out.println("Erro de conexão com o GRID.");
+				e.printStackTrace();
+			}
+		}
+
 		driver.manage().window().maximize();
 		return driver;
 	}
--- ProjetoBasico/src/main/java/br/com/lanza/core/Propriedades.java
+++ ProjetoBasico/src/main/java/br/com/lanza/core/Propriedades.java
@@ -4,12 +4,19 @@ public class Propriedades {
 
 	public static boolean FECHAR_BROWSER = false;
 
-	public static Browsers browser = Browsers.CHROME;
-	
-	public static String NOME_CONTA_ALTERADA = "Conta Alterada " + System.nanoTime();
+	public static Browsers BROWSER = Browsers.CHROME;
+
+	// public static String NOME_CONTA_ALTERADA = "Conta Alterada " +
+	// System.nanoTime();
+
+	public static TipoExecucao TIPO_EXECUCAO = TipoExecucao.LOCAL;
 
 	public enum Browsers {
 		CHROME, FIREFOX
+	}
+
+	public enum TipoExecucao {
+		LOCAL, GRID, NUVEM
 	}
 
 }
